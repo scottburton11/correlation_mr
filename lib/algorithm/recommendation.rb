82419107bd3513a1fc8e98dc8da0c51050ae2157
Scott Burton <scottburton11@gmail.com>
2010-07-16 14:02:31 -0700

    move recommendation.map into js file

--- lib/algorithm/recommendation.rb
+++ lib/algorithm/recommendation.rb
@@ -7,26 +7,26 @@ class Recommendation
     end
     
     def map
-      # File.read(File.expand_path(File.dirname(__FILE__) + "/js/#{self.name.downcase}_map.js")).gsub(/[\n\t]/, " ")      
-      %Q#function() {
-      	var location_scores = {};
-      	var similarities = {}
-      	var me = this;
-      	 db.people.find({name: {$in: this.follows}}).forEach(function(person){
-      		for(location in person.reviews) {
-      			if(location_scores[location] == undefined){
-              location_scores[location] = [];
-      			}
-      			if(similarities[location] == undefined){
-      			  similarities[location] = [];
-      			}
-      			location_scores[location].push((person.reviews[location] * me.scores[person.name]));
-      			similarities[location].push(me.scores[person.name]);
-      		}
-       	 });
-        emit(me._id, {location_scores: location_scores, similarities: similarities});
-      };
-      #
+      File.read(File.expand_path(File.dirname(__FILE__) + "/js/#{self.name.downcase}_map.js")).gsub(/[\n\t]/, " ")      
+      # %Q#function() {
+      #   var location_scores = {};
+      #   var similarities = {}
+      #   var me = this;
+      #    db.people.find({name: {$in: this.follows}}).forEach(function(person){
+      #     for(location in person.reviews) {
+      #       if(location_scores[location] == undefined){
+      #         location_scores[location] = [];
+      #       }
+      #       if(similarities[location] == undefined){
+      #         similarities[location] = [];
+      #       }
+      #       location_scores[location].push((person.reviews[location] * me.scores[person.name]));
+      #       similarities[location].push(me.scores[person.name]);
+      #     }
+      #    });
+      #   emit(me._id, {location_scores: location_scores, similarities: similarities});
+      # };
+      # #
     end
     
     def reduce
